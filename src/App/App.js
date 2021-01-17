d39c218bb8c1788d7123ed2a582e7ed5df3757b1
Richard Hounslow <rhounslow@gmail.com>
2021-01-17 23:17:23 +1100

    messing with styled components

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import WelcomeMessage from "./WelcomeMessage";
 import AppLayout from "./AppLayout";
+import styled from 'styled-components';
+
+const MyButton = styled.div`
+    color: green;    
+`
 
 function App() {
   return (
       <AppLayout>
         <WelcomeMessage/>
+        <MyButton> Hello </MyButton>
       </AppLayout>
   );
 }
