f244293c70bb6087f8fb4034697c41c39f4a6885
Richard Hounslow <rhounslow@gmail.com>
2021-01-19 09:25:11 +1100

    Appbar buttons

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,21 +1,42 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 // rem: back ticks are template literals,
 // allowing embedded expressions ${expression}
 // Diff ways of styling in React https://www.youtube.com/watch?v=NMiEREulVLc
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`
+
 const Bar = styled.div`
   display: grid; // css grid
   grid-template-columns: 180px auto 100px 100px;
 `;
 
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    color: blue;  
+  `
+  }
+`
+
+function ControlButton({name, active}) {
+    return(
+        <ControlButtonElem active={active}>
+            {name}
+        </ControlButtonElem>
+    )
+}
+
 export default function () {
   return (
     <Bar>
-      <div>CryptoDash</div>
+      <Logo>CryptoDash</Logo>
       <div></div>
-      <div>Dashboard</div>
-      <div>Settings</div>
+      <ControlButton active name="Dashboard"></ControlButton>
+      <ControlButton name="settings">Settings</ControlButton>
     </Bar>
   );
 }
