f9c733d423c5ce3e2dde62997f9df5835d08101c
Richard Hounslow <rhounslow@gmail.com>
2021-01-18 14:24:30 +1100

    reverse messing with styled components w/ props (has to be 4.0.2)

--- src/App/App.js
+++ src/App/App.js
@@ -3,19 +3,10 @@ import WelcomeMessage from "./WelcomeMessage";
 import AppLayout from "./AppLayout";
 import styled, {css} from 'styled-components';
 
-const MyButton = styled.div`
-    color: green;
-    ${props => props.primary && css`
-      color: palevioletred;
-    `}
-`
-
 function App() {
   return (
       <AppLayout>
         <WelcomeMessage/>
-        <MyButton> Hello </MyButton>
-          <MyButton primary> Hello </MyButton>
       </AppLayout>
   );
 }
