32109c01812393bfd75a3e3959e97aade1c86e71
Richard Hounslow <rhounslow@gmail.com>
2021-01-22 13:23:57 +1100

    Add page component

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
 
 export default function () {
   return (
-    <div>
-      <WelcomeMessage /> <ConfirmButton />
-    </div>
+    <Page name="settings">
+      <WelcomeMessage />
+      <ConfirmButton />
+    </Page>
   );
 }
