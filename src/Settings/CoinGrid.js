c4db6a2818828d517441e193250fb4de99db9c48
Richard Hounslow <rhounslow@gmail.com>
2021-01-27 10:29:51 +1100

    Add Styles.

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -4,18 +4,19 @@ import { AppContext } from "../App/AppProvider";
 
 export const ConGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr) ;
+  grid-template-columns: repeat(5, 1fr);
 `;
 
 export default function () {
-    return <AppContext.Consumer>
-        {
-            ({coinList}) => <ConGridStyled>
-                {Object.keys(coinList).map(coinKey =>
-                    <div> {coinKey} </div>
-                )}
-            </ConGridStyled>
-        }
+  return (
+    <AppContext.Consumer>
+      {({ coinList }) => (
+        <ConGridStyled>
+          {Object.keys(coinList).map((coinKey) => (
+            <div> {coinKey} </div>
+          ))}
+        </ConGridStyled>
+      )}
     </AppContext.Consumer>
+  );
 }
-
