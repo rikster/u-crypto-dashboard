df07330f80e616fa353e9264cddcff632d08f162
Richard Hounslow <rhounslow@gmail.com>
2021-02-04 14:06:11 +1100

    create coingrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { AppContext } from "../App/AppProvider";
+import {Tile} from '../Shared/Tile'
+import {SelectableTile} from "../Shared/Tile";
 
 export const ConGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px; 
 `;
 
 export default function () {
@@ -13,7 +16,7 @@ export default function () {
       {({ coinList }) => (
         <ConGridStyled>
           {Object.keys(coinList).map((coinKey) => (
-            <div> {coinKey} </div>
+            <SelectableTile> {coinKey} </SelectableTile>
           ))}
         </ConGridStyled>
       )}
